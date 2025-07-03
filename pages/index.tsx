65c2fc559e919cea577267229c540031dfdf5d22
badamsonAPI <bryanadamson88@gmail.com>
2025-07-03 13:06:14 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Layout from '../components/Layout';
+import React from 'react';
 
 export default function Home() {
   return (
-    <Layout>
-      <h1>Welcome to PokerAced!</h1>
-      <p>Compare the best poker sites, bonuses, and legal info worldwide.</p>
-    </Layout>
+    <div style={{ textAlign: 'center', padding: '4rem' }}>
+      <h1>Welcome to PokerAced</h1>
+      <p>The #1 resource for top poker sites, bonuses, and expert guides.</p>
+    </div>
   );
 }
